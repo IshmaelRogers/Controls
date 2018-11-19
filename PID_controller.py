d6656614b1cf82adf9ffe91fdb64c33cce1283b4
Ishmael Rogers <41645540+IshmaelRogers@users.noreply.github.com>
2018-11-18 21:06:09 -0500

    Update PID_controller.py

--- PID_controller.py
+++ PID_controller.py
@@ -1,11 +1,22 @@
 class PIDController:
-    def __init__(self, kp = 0.0, ki = 0.0,  kd = 0.0, start_time = 0):
+    def __init__(self, kp = 0.0, ki = 0.0, kd = 0.0, max_windup = 20,
+            start_time = 0, alpha = 1., u_bounds = [float('-inf'), float('inf')]):
         
         # The PID controller can be initalized with a specific kp value
         # ki value, and kd value
         self.kp_ = float(kp)
         self.ki_ = float(ki)
         self.kd_ = float(kd)
+        
+        # Set max wind up
+        self.max_windup_ = float(max_windup)
+        
+        # Set alpha for derivative filter smoothing factor
+        self.alpha = float(alpha) 
+        
+        # Setting control effort saturation limits
+        self.umin = u_bounds[0]
+        self.umax = u_bounds[1]
 
         # Store relevant data
         self.last_timestamp_ = 0.0
@@ -19,17 +30,33 @@ class PIDController:
         self.u_i = [0]
         self.u_d = [0]
 
+    # Add a reset function to clear the class variables
+    def reset(self):
+        self.set_point_ = 0.0
+        self.kp_ = 0.0
+        self.ki_ = 0.0
+        self.kd_ = 0.0
+        self.error_sum_ = 0.0
+        self.last_timestamp_ = 0.0
+        self.last_error_ = 0
+        self.last_last_error_ = 0
+        self.last_windup_ = 0.0
+
     def setTarget(self, target):
         self.set_point_ = float(target)
 
     def setKP(self, kp):
         self.kp_ = float(kp)
-        
+
     def setKI(self, ki):
         self.ki_ = float(ki)
-       
+
     def setKD(self, kd):
         self.kd_ = float(kd)
+
+    # Create function to set max_windup_
+    def setMaxWindup(self, max_windup):
+        self.max_windup_ = int(max_windup)
 
     def update(self, measured_value, timestamp):
         delta_time = timestamp - self.last_timestamp_
@@ -45,12 +72,20 @@ class PIDController:
 
         # Sum the errors
         self.error_sum_ += error * delta_time
-        
-        # Calculate the delta_error
+
+        # Find delta_error
         delta_error = error - self.last_error_
         
-        # Update the past error with the current error
+        # Update the past error
         self.last_error_ = error
+        
+        # Address max windup
+        ########################################
+        if self.error_sum_ > self.max_windup_:
+            self.error_sum_ = self.max_windup_
+        elif self.error_sum_ < -self.max_windup_:
+            self.error_sum_ = -self.max_windup_
+        ########################################
         
         # Proportional error
         p = self.kp_ * error
@@ -58,12 +93,24 @@ class PIDController:
         # Integral error
         i = self.ki_ * self.error_sum_
        
-        # Derivative error 
-        d = self.kd_ * (delta_error / delta_time)
-
+        # Recalculate the derivative error here incorporating 
+        # derivative smoothing!
+        ########################################
+        d = self.kd_ * (self.alpha * delta_error / delta_time + \
+            (1 - self.alpha)  * self.last_error_ / delta_time)
+        ########################################
+        
         # Set the control effort
         u = p + i + d
         
+        # Enforce actuator saturation limits
+        ########################################
+        if u > self.umax:
+            u = self.umax
+        elif u < self.umin:
+            u = self.umin
+        ########################################
+    
         # Here we are storing the control effort history for post control
         # observations. 
         self.u_p.append(p)
